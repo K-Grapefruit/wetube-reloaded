f80b2f2dca63c13ee3f5195bb1f00e9cd88f1363
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2021-10-26 22:33:42 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -8,4 +8,4 @@ import app from "./server";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log("Server listening on port 4000"));
+app.listen(PORT, () => console.log(`Mooyaho listening on port ${PORT}`));
